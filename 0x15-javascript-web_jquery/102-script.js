e7239d5b2dc0890a3f43ef9e54639dfc4875b413
okiogutu <okiogutu@gmail.com>
2023-10-04 15:39:18 -0700

    copy ied

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,3 @@
+$(document).ready(function () {
+	$("INPUT#btn_translate").click(function () {
+		const language_code = $("INPUT#language_code").val();
---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,3 @@
+$(document).ready(function () {
+	$("DIV#toggle_header").click(function () {
+		$("header").toggleClass("red green");
